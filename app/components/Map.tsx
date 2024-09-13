e3368e34f7f47061ffbe10e4ecebe9c9123d4802
SAWAPANDEEP <78143025+Sawapandeep@users.noreply.github.com>
2024-09-13 16:00:07 +0530

    map imlemented

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -1,9 +1,71 @@
-import React from 'react'
+'use client';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import React from 'react';
+// Import Library type from @googlemaps/js-api-loader
 
-const Map = () => {
-    return (
-        <div>Map</div>
-    )
-}
+// Use the Library type from @googlemaps/js-api-loader
+const defaultTarget = { lat: 32.26325, lng: 75.59422 };
 
-export default Map
+type GoogleMapInstance = google.maps.Map;
+const Map: React.FC = () => {
+
+
+    const options = {
+        mapId: "5e138c66d669e81a",
+        mapTypeControl: false,
+    }
+    const { isLoaded } = useJsApiLoader({
+        googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!
+    })
+
+
+    if (!isLoaded) return <div><span className="loading loading-infinity bg-white loading-lg"></span></div>;
+
+
+    return isLoaded ? (
+        <div className="h-[90vh] w-[90vw]  rounded-3xl overflow-hidden mx-auto">
+            <GoogleMap
+                mapContainerClassName='h-[100%] w-[100%]'
+                options={options}
+                zoom={14}
+                center={defaultTarget}
+            // onLoad={onLoad}
+            // onUnmount={onUnmount}
+            >
+                {/* <Marker position={defaultTarget} /> */}
+            </GoogleMap>
+        </div >
+
+
+    ) : <></>
+
+};
+
+export default Map;
+
+
+// import { GoogleMap, Marker, useLoadScript } from '@react-google-maps/api';
+// import { Library } from '@react-google-maps/api/dist/utils/make-load-script-url'
+// import React from 'react';
+
+// const libraries: Library[] = ['places'];
+
+// const Map: React.FC = () => {
+//     const { isLoaded } = useLoadScript({
+//         googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!,
+//         libraries,
+//     });
+//     const defaultTarget = { lat: 32.26325, lng: 75.59422 };
+
+//     if (isLoaded) return <div><span className="loading loading-infinity loading-lg"></span></div>
+//     return (
+//         <GoogleMap
+//             mapContainerClassName='h-[60vh] w-[60vw]'
+//             zoom={14}
+//             center={defaultTarget}>
+//             <Marker position={defaultTarget} />
+//         </GoogleMap>
+//     );
+// };
+
+// export default Map
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
-import html from "./components/Map";
+import Map from "@/app/components/Map";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <html />
-      </main>
-    </div>
+
+    <main >
+      <h1 className="text-center text-2xl font-bold mx-auto">Google Maps Integration</h1>
+      <Map />
+    </main>
+
   );
 }
